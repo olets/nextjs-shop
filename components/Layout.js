51dc882a062644c2b8acb14e2d2bac62880f7cea
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-03 18:44:27 -1000

    refactor(layout): use shared layout

---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Header from './Header'
+
+export const siteTitle = 'Next.js Store'
+
+export default function Layout({ children, title = siteTitle }) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Store in Next.js"/>
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={title} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <Header></Header>
+
+      <main className="flex-1 p-10 text-center">
+        <h1 className="mb-10 text-2xl">
+        {title}
+        </h1>
+        
+        {children}
+      </main>
+
+      <footer className="border-t p-8 w-full">
+        by @olets from the guide at https://blog.logrocket.com/building-a-next-js-shopping-cart-app/
+      </footer>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import CategoryCard from '../components/CategoryCard'
 import Header from '../components/Header'
+import Layout, { siteTitle } from '../components/Layout'
 
 const categories = [
   {
@@ -19,33 +20,20 @@ const categories = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <h1 className="mb-10 text-2xl">
+        Next.js Store
+      </h1>
 
-      <Header></Header>
+      <h2 className="mb-10 text-xl">Categories</h2>
 
-      <main className="flex-1 p-10 text-center">
-        <h1 className="mb-10 text-2xl">
-          Next.js Store
-        </h1>
-
-        <h2 className="mb-10 text-xl">Categories</h2>
-
-        <ul className="grid grid-cols-3 gap-5">
-          {categories.map(({ imageSrc, name }, index) => (
-            <li key={index}>
-              <CategoryCard imageSrc={imageSrc} name={name} />
-            </li>
-          ))}
-        </ul>
-      </main>
-
-      <footer className="border-t p-8 w-full">
-        by @olets from the guide at https://blog.logrocket.com/building-a-next-js-shopping-cart-app/
-      </footer>
-    </div>
+      <ul className="grid grid-cols-3 gap-5">
+        {categories.map(({ imageSrc, name }, index) => (
+          <li key={index}>
+            <CategoryCard imageSrc={imageSrc} name={name} />
+          </li>
+        ))}
+      </ul>
+    </Layout>
   )
 }
