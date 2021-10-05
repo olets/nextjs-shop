e1dd88ae719ebf03c0b1df82fbe83aabb5d89619
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-04 16:09:33 -1000

    feat(header): include cart size

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,30 @@
 import cx from 'classnames'
 import Link from 'next/link'
+import { useSelector } from 'react-redux'
 import { useRouter } from 'next/router'
-
-const headerLinks = [
-  {
-    href: '/',
-    text: 'Home',
-  },
-  {
-    href: '/shop',
-    text: 'Shop',
-  },
-  {
-    href: '/cart',
-    text: 'Cart',
-  }
-]
 
 const Header = () => {
   const router = useRouter()
+  const cart = useSelector((state) => state.cart)
+
+  const getItemsCount = () => {
+    return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
+  }
+
+  const headerLinks = [
+    {
+      href: '/',
+      text: 'Home',
+    },
+    {
+      href: '/shop',
+      text: 'Shop',
+    },
+    {
+      href: '/cart',
+      text: `Cart (${getItemsCount()})`,
+    }
+  ]
 
   return (
     <header className="p-4 w-full">
