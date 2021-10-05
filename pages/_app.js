47fc91d4c210e480f7cec873e737502f697e227e
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-04 16:29:27 -1000

    feat(store): persist across browser refresh...
    Method described at
    - https://stackoverflow.com/a/63818121/1241736
    - https://edvins.io/how-to-use-redux-persist-with-redux-toolkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import { Provider } from 'react-redux'
 import store from '../redux/store'
 import 'tailwindcss/tailwind.css'
 
+let persistor = persistStore(store);
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from "redux"
+import { persistReducer } from 'redux-persist'
 import { cartReducer } from './cart.slice'
 
-const reducer = {
+const reducers = combineReducers({
   cart: cartReducer,
-}
+});
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 const store = configureStore({
-  reducer,
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
 })
 
 export default store
