46f52761259b297c1474c3885ddcd322d0e5a74e
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-10 17:25:11 -1000

    feat(product card): link category to category page

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -10,13 +10,6 @@ const ProductCard = ({ product }) => {
 
   return (
     <div className="group shadow relative pb-4">
-
-      <Link href={`/product/${product.name}`}>
-        <a className="absolute top-0 left-0 w-full h-full">
-          <span className="sr-only">View product page</span>
-        </a>
-      </Link>
-
       <Image
         className="group-hover:scale-125 transition-transform"
         src={product.imageSrc}
@@ -28,19 +21,27 @@ const ProductCard = ({ product }) => {
 
       <dl>
         <dt>Category: </dt>
-        <dd>{product.category}</dd>
+        <dd>
+          <Link href={`/category/${product.category}`}>
+            <a>{product.category}</a>
+          </Link>
+        </dd>
         <dt>Price:</dt>
         <dd>{CurrencyFormatter.format(product.price)}</dd>
       </dl>
+
+      <Link href={`/product/${product.name}`}>
+        <a className="block">
+          See details
+        </a>
+      </Link>
       
-      <div className="inline-block p-2 relative">
-        <button
-          onClick={() => dispatch(addToCart(product))}
-          className="hover:shadow hover:bg-blue-700 focus:shadow focus:bg-blue-700 transition-colors bg-blue-400 p-8 py-4 text-white rounded"
-        >
-          Add to Cart
-        </button>
-      </div>
+      <button
+        onClick={() => dispatch(addToCart(product))}
+        className="hover:shadow hover:bg-blue-700 focus:shadow focus:bg-blue-700 transition-colors bg-blue-400 p-8 py-4 text-white rounded"
+      >
+        Add to Cart
+      </button>
     </div>
   )
 }
