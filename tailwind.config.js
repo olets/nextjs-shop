5cac6dd557cc485aac91a055ae0706e0ffc6f1aa
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2021-10-10 16:57:21 -1000

    chore(tailwind): clear warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'class'
   theme: {
     extend: {
       colors: {
